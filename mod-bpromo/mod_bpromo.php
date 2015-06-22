a7d81f904fe802be5e1f4c7bc536b3b20067c0f6
Yair <yair@billrun.net>
2015-06-22 17:36:18 +0300

    Joomla module - BPromo - first release

---
+++ mod-bpromo/mod_bpromo.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author		
+ * @copyright	
+ * @license		
+ */
+
+defined("_JEXEC") or die("Restricted access");
+
+$scriptContent = "
+	setTimeout(function(){
+    	document.getElementById('b-promo-out').style.display = 'none';
+   	}, " . $params['timer'] * 1000 . ");
+";
+$doc =& JFactory::getDocument();
+$doc->addScriptDeclaration( $scriptContent );
+$doc->addStyleSheet( JURI::base() . "/modules/mod_bpromo/css/style.css");
+
+// Include the syndicate functions only once
+require_once __DIR__ . '/helper.php';
+$item = ModBpromoHelper::getItem($params);
+
+require JModuleHelper::getLayoutPath('mod_bpromo', $params->get('layout', 'default'));
