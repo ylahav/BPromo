a8d8d747f49d0674cce68aecf7d0e2e2261d5aa7
ylahav <yair.lahav@gmail.com>
2015-07-17 18:15:29 +0300

    fixed bug (duplicate code by mistake)

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,24 +8,6 @@
 defined("_JEXEC") or die("Restricted access");
 ?>
 
-    <div id='b-promo-out' class='container-fluid'>
-        <div id='b-promo-in'>
-            <a href="<?php echo $item['url']; ?>" <?php echo ($params[ 'onepage'])? '': 'target="_blank"'; ?>>
-		<img src="<?php echo $item['image']; ?>" alt="promo" />
-	</a>
-        </div>
-    </div>
-
-    <?php
-/**
- * @author
- * @copyright
- * @license
- */
-
-defined("_JEXEC") or die("Restricted access");
-?>
-
 <div id='b-promo-out' class='container-fluid'>
     <div id='b-promo-in'>
         <a href="<?php echo $item['url']; ?>" <?php echo ($params[ 'samepage'])? '': 'target="_blank"'; ?>>
